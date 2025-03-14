709735e557fa85cced6aea6d838548dfffbf79ea
Ashu Adhikari <ashua@sfu.ca>
2025-03-14 07:44:41 -0400

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    base: '/info/', // Ensure this matches your GitHub repo name
     plugins: [react()],
 });
